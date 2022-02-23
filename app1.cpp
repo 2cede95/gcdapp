d8e0c447244c26959d014f78c90ea90189cc6c03
2cede95 <96794368+2cede95@users.noreply.github.com>
2022-02-23 21:02:33 +0000

    Add files via upload

---
+++ app1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int singleDivision(int a, int b, int c, int d){
+    int divisor{a/b};
+    int remainder{a%b};
+    std::cout << b << " x " << divisor << " + " << remainder << " = " << a << '\n';
+        if (remainder > 0){
+            singleDivision(b, remainder, c, d);
+}
+        else{
+        std::cout << "gcd(" << c << ", " << d << ") = " << b;
+}
+    return 0;
+}
+
+int main(){
+    int a{};
+    int b{};
+    std::cin >> a;
+    std::cin >> b;
+    int c{a};
+    int d{b};
+    singleDivision(a, b, c, d);
+    return 0;
+}
